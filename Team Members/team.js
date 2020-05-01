757a566e1078a0c135fa976ee162fe149cf6cc4b
Minaya Yagubova <61638622+mnyx7@users.noreply.github.com>
2020-05-02 00:59:14 +0400

    Update team.js

--- Team Members/team.js
+++ Team Members/team.js
@@ -1,31 +1,29 @@
 window.onload = function () {
-
-    fetch('https://gdg-ms-team.herokuapp.com/api/members')
+    fetch('https://ms-team-gdg.herokuapp.com/api/members')
         .then(res => res.json())
         .then(json => {
-            var  team = document.getElementById('team')[0]
+            var team = document.getElementsByClassName('team')[0]
             renderMember(json)
         })
 
-
-        const renderMember = members => {
-        members.forEach(member => {
-            
-
+        
+    const renderMember = records => {
+        records.forEach(record => {
             team.innerHTML += `
             <div class="member">
-                    <div class="photoOfMember">
-                    <img src="${member.photo}" alt="${member.position}" />
-                        <h1 class="name">${member.firstName} ${member.lastName}</h1>
-                        <p class="position">${member.position}</p>
+                    <div class="face photo">
+                    <img id="photo" alt="${record.position}" />
+                        <h1 class="name">${record.firstName} ${record.lastName}</h1>
+                        <p class="position">${record.position}</p>
+                        <p>${record.email}</p>
                         <div class="m-icons">
-                            <a href="mailto:${member.email}" class="fas fa-envelope-square mymail"></a>
-                            <a href="${member.linkedin}" class="fab fa-linkedin mylink"></a>
-                            <a href="${member.github}" class="fab fa-github-square mygit"></a>
+                            <a href="mailto:${record.email}" target="_blank" class="fas fa-envelope-square mymail"></a>
+                            <a href="${record.linkedin}" target="_blank"  class="fab fa-linkedin mylink"></a>
+                            <a href="${record.github}" target="_blank"  class="fab fa-github-square mygit"></a>
                         </div>
                     </div>
-                    <div class="factAboutMember">
-                        <p>Fact Fact Fact</p>
+                    <div class="face info">
+                        <p>Fact ${record.fact}</p>
                     </div>
                 </div>`
 
